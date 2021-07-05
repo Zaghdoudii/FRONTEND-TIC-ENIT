be998c196d4ed6a53525659df0c72c7f9d083022
dabbebi <laid.dabbabi@etudiant-enit.utm.tn>
2021-07-05 18:58:02 +0100

    deploy

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
+
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/frontend-ticenit'));
+app.use(express.static('./dist/'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/frontend-ticenit/'}),
+    res.sendFile('index.html', {root: 'dist/'}),
 );
 
 // Start the app by listening on the default Heroku port
